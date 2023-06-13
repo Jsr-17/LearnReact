990b0169c248d8ea842a1bd38f415dd0ad2b3b56
Jsr017 <123515664+Jsr017@users.noreply.github.com>
2023-06-13 17:02:07 +0200

    Custom CounterHook

---
+++ Hooks/src/01-useState/CounterCustomhook.jsx
@@ -0,0 +1,14 @@
+export const CounterCustomHook=()=>{
+
+    
+
+    return(
+        <>
+            <h1>Counter with Hook:</h1>
+            <hr />
+            <button className="btn btn-primary">+1</button>
+            <button className="btn btn-primary">Reset</button>
+            <button className="btn btn-primary">-1</button>
+        </>
+    )
+}
--- Hooks/src/main.jsx
+++ Hooks/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HooksApp } from './HooksApp';
 import { CounterApp } from './01-useState/CounterApp';
+import { CounterCustomHook } from './01-useState/CounterCustomhook';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <HooksApp></HooksApp> */}
-    <CounterApp></CounterApp>
+    {/* <CounterApp></CounterApp> */}
+    <CounterCustomHook></CounterCustomHook>
   </React.StrictMode>,
 )
